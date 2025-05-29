3cc3d40d5cb083f8caeabb7a19088cd2fcfee823
Burak Varıcı <32434759+bvarici@users.noreply.github.com>
2025-05-28 22:31:40 -0400

    Update l_scale_i.py - refined algorithm
    LSCALE-I algorithm is refined in the update arxiv/v4 (Feb. 2025).

--- LSCALE-I/l_scale_i.py
+++ LSCALE-I/l_scale_i.py
@@ -4,9 +4,7 @@ Setting: X = G.Z, i.e. linear transform.
 single-node interventions.
 
 Paper: Score-based Causal Representation Learning: Linear and General Transformations
-(https://arxiv.org/abs/2402.00849)
-
-TODO: Add some file-wide comments.
+(https://arxiv.org/abs/2402.00849), published at JMLR 05/2025.
 """
 
 __all__ = ["l_scale_i"]
@@ -25,26 +23,42 @@ import numpy.typing as npt
 
 import utils
 
-
 def l_scale_i(
     x_samples: npt.NDArray[np.floating],
     dsx_samples: npt.NDArray[np.floating],
     hard_intervention: bool,
-    full_rank_scores: bool,
     hard_graph_postprocess: bool,
-    atol_eigv: float,
-    atol_orth: float,
-    atol_edge: float,
+    soft_graph_postprocess: bool,
+    full_rank_scores: bool,
+    ATOL_EIGV: float,
+    ATOL_EDGE: float,
 ) -> tuple[
     npt.NDArray[np.int_],
     npt.NDArray[np.bool_],
     npt.NDArray[np.floating],
+    npt.NDArray[np.floating],
     tuple[
+        npt.NDArray[np.int_],
         npt.NDArray[np.bool_],
+        npt.NDArray[np.floating],
         npt.NDArray[np.floating],
     ]
     | None,
 ]:
+    '''
+    LSCALE-I (Algorithm 1) in the paper.
+
+    Inputs:
+    - x_samples: (n + 1, nsamples, d, 1) array of samples. First dimension is observational, the rest are n single-node interventions.
+    - dsx_samples: (n, nsamples, d, 1) array of samples. Score differences for n single-node interventions w.r.t. observational environment. Samples from the obs. environment are used.
+    - hard_intervention: True if the interventions are hard, False if they are soft.
+    - hard_graph_postprocess: True if the hard int. graph estimate should be postprocessed with the soft int. graph estimate.
+    - soft_graph_postprocess: True if the full-rank soft int. graph estimate should be postprocessed with the vanilla soft int. graph estimate.
+    - full_rank_scores: True if the score differences are known to be full-rank, False otherwise. (see Assumption 2 of the paper)
+    - ATOL_EIGV: (only for full-rank scores case) Tolerance for eigenvalue thresholding.
+    - ATOL_EDGE: Tolerance for edge thresholding in graph estimate.
+    '''
+
     assert dsx_samples.ndim == 4 and dsx_samples.shape[3] == 1
     n, nsamples, d, _ = dsx_samples.shape
     assert x_samples.shape == (n + 1, nsamples, d, 1)
@@ -52,8 +66,7 @@ def l_scale_i(
     # Preprocessing:
     # x and relevant parts of dsx lives in `n` dimensional column space of the decoder.
     # Find this subspace and write x and dsx samples in this basis.
-    # Note that `n` random samples almost surely suffice for this task. I use `n + d`
-    # samples just in case.
+    # Note that `n` random samples almost surely suffice for this task. We use `n + d` samples just in case.
     x_cov = utils.cov(x_samples[:, : n + d, :, 0])
     _, dec_svec = np.linalg.eigh(np.sum(x_cov, 0))
     dec_colbt = dec_svec[:, -n:].T
@@ -65,204 +78,225 @@ def l_scale_i(
     dsx_n_cor = utils.cov(dsx_n_samples[..., 0], center_data=False)
 
     # Run algorithm steps
-    top_order = _obtain_causal_order(dsx_n_cor)
-    hat_g_s, hat_enc_n_s = _minimize_score_variations(
-        dsx_n_cor,
-        top_order,
-        full_rank_scores,
-        atol_eigv,
-        atol_orth,
-    )
+    hat_enc_n_s = _estimate_encoder(dsx_n_cor)
+    hat_dec_n_s = np.linalg.pinv(hat_enc_n_s)
+    # optional: normalize the decoder
+    #hat_dec_n_s /= np.max(np.abs(hat_dec_n_s), 0)
+    #hat_enc_n_s = np.linalg.pinv(hat_dec_n_s)
 
+    A = dec_colbt.T @ hat_dec_n_s
+    hat_dec_n_s = dec_colbt @ (A / np.max(np.abs(A), 0))
+    hat_enc_n_s = np.linalg.pinv(hat_dec_n_s)
+
+    # Compute the "estimated latent" score diffs
+    dsz_cor_s = hat_dec_n_s.T @ dsx_n_cor @ hat_dec_n_s
+    # Estimate the graph
+    hat_g_s, top_order_s = _estimate_graph(dsz_cor_s, ATOL_EDGE)
     # Transform the encoder estimates back up to `d` dimensions
     hat_enc_s = hat_enc_n_s @ dec_colbt
-
-    # Optional hard intervention routine
+    soft_results = (hat_g_s, hat_enc_s, top_order_s, dsz_cor_s)
+    # placeholders
     hard_results = None
-    if hard_intervention:
-        hat_g_h, hat_enc_n_h, dshatz_cor = _unmixing_procedure(
-            x_n_cov,
-            dsx_n_cor,
-            top_order,
-            hat_enc_n_s,
-            atol_edge,
-        )
+    soft_full_rank_results = None
 
+    # See Assumption 2 of the paper: if the score differences are known to be full-rank, then we can use a slightly different algorithm to get better estimates.
+    if full_rank_scores == True:
+        hat_g_f, hat_enc_n_f = l_scale_i_full_rank_scores(dsx_n_cor, hat_g_s, top_order_s, hat_enc_n_s, ATOL_EIGV)
+        if soft_graph_postprocess == True:
+            hat_g_f *= hat_g_s
+
+            
+        hat_dec_n_f = np.linalg.pinv(hat_enc_n_f)
+        # WLOG, normalize the decoder
+        hat_dec_n_f /= np.max(np.abs(hat_dec_n_f), 0)
+        # Transform the encoder estimates back up to `d` dimensions
+        hat_enc_f = hat_enc_n_f @ dec_colbt
+        # Compute the "estimated latent" score diffs
+        dsz_cor_f = hat_dec_n_f.T @ dsx_n_cor @ hat_dec_n_f
+        soft_full_rank_results = (hat_g_f, hat_enc_f, top_order_s, dsz_cor_f)
+    
+    # Optional: hard intervention routine. If the interventions are hard, then we can refine the estimates further.
+    if hard_intervention:
+        hat_enc_n_h, hat_g_h, top_order_h, dsz_cor_h = _unmixing_cov(x_n_cov, dsx_n_cor, top_order_s, hat_enc_n_s , hat_g_s, ATOL_EDGE)
         # Transform the encoder estimates back up to `d` dimensions
         hat_enc_h = hat_enc_n_h @ dec_colbt
-        hard_results = (hat_g_h, hat_enc_h, dshatz_cor)
-
+        
         if hard_graph_postprocess:
             hat_g_h *= hat_g_s
 
-    return top_order, hat_g_s, hat_enc_s, hard_results
+        hard_results = (hat_g_h, hat_enc_h, top_order_h, dsz_cor_h)
+
+    return soft_results, hard_results, soft_full_rank_results
 
 
-def _obtain_causal_order(
+def _estimate_encoder(
     dsx_cor: npt.NDArray[np.floating],
-) -> npt.NDArray[np.int_]:
-    n, _, _ = dsx_cor.shape
-    logging.info(f"Starting `_obtain_causal_order`.")
-
-    vt = list(range(n))
-    top_order = np.arange(n)
-
-    # When we find a `k` that increases the kernel dimension, record and continue
-    for t in range(n - 1, 0, -1):
-        logging.debug(f"{t = }, {vt = }")
-        min_eigval_l = np.ones(n) * np.finfo(np.floating).max
-        for k in vt:
-            vt_new = list[int](i for i in vt if i != k)
-            min_eigval_l[k] = np.linalg.eigvalsh(np.sum(dsx_cor[vt_new], 0))[
-                n - t - 1
-            ]
-        youngest_node = np.argmin(min_eigval_l)
-        top_order[t] = youngest_node
-        vt = list[int](i for i in vt if i != youngest_node)
-
-    # The remaining entry is the oldest
-    top_order[0] = vt[0]
-    return top_order
-
-
-def _minimize_score_variations(
-    dsx_cor: npt.NDArray[np.floating],
-    top_order: npt.NDArray[np.int_],
-    full_rank_scores: bool,
-    atol_eigv: float,
-    atol_orth: float,
-) -> tuple[npt.NDArray[np.bool_], npt.NDArray[np.floating]]:
+) -> npt.NDArray[np.floating]:
     n, d, _ = dsx_cor.shape
-    logging.info(f"Starting `_minimize_score_variations`.")
-    logging.debug(f"{atol_eigv = }, {atol_orth = }")
+    logging.info(f"Starting `_estimate_encoder`.")
+    """Estimate the encoder.
+
+    Stage L1 of Algorithm 1 in the paper.
+
+    Inputs:
+    - dsx_cor: (n, d, d) array of score differences covariance matrices for n single-node interventions w.r.t. observational environment. See R_X matrices in the paper: dsx_cor[i] = R_X^i.
+
+    The original algorithm randomly picks a vector from column space of `rxs[m]` and assigns it to the m-th row of the encoder.
+
+    Here, we use the eigenvector corresponding to the largest eigenvalue to make the algorithm deterministic."""
 
     # We disregard any possible "trivial" null space components:
     # If the original decoder has a null space, _all_ dsx_cor null spaces include it.
     assert n == d
 
-    # We will refine this estimate
-    hat_enc_s_pinv_t = np.zeros((n, d))
+    encoder_hat = np.zeros((n, d))
+    for m in range(n):
+        # Estimate the encoder row by row from score differences
+        _, eigvectors = np.linalg.eigh(dsx_cor[m])
+        encoder_hat[m] = eigvectors[:, -1]
 
-    # This DAG can correspond to two different things:
-    # 1. An estimate of the transitive closure---when the score differences are not,
-    #    a priori, known to be full rank, or
-    # 2. An estimate of the graph itself.
-    # In either case, it is used to exclude environments from the minimization objective.
-    hat_g_s = np.zeros((n, n), dtype=np.bool_)
+    return encoder_hat
 
-    # V_t sets generated by `_obtain_causal_order`
-    vts = [top_order[: t + 1] for t in range(n)]
+def _estimate_graph(dsz_cor: npt.NDArray[np.floating],ATOL_EDGE:float
+                    ) -> npt.NDArray[np.bool_]:
+    logging.info(f"Starting `_estimate_graph`.")
+    """Estimate the graph.
 
-    for t in range(n - 1, -1, -1):
-        logging.debug(f"{t = }, {vts[t] = }")
+    Stage L2 of Algorithm 1 in the paper.
 
-        # Find vt's column space
-        cm = np.sum(dsx_cor[vts[t]], 0)
-        cm_eig = np.linalg.eigh(cm)
-        cm_eigval: npt.NDArray[np.floating] = cm_eig.eigenvalues
-        cm_eigvec: npt.NDArray[np.floating] = cm_eig.eigenvectors
-        cm_rank = np.sum(cm_eigval > atol_eigv, dtype=np.int_)
-        logging.debug(f"Rank of vt column space: {cm_rank}")
-        vt_colb = cm_eigvec[:, -(t + 1) :]
+    Given that the encoder is estimated (at worst) up to mixing with parents, the corresponding score differences are non-zero only at locations corresponding to the intervened node's ancestors."""
+    # Root-mean-square of the estimated latent score diffs
+    # can be computed from correlation matrices as follows
+    n = dsz_cor.shape[0]
 
-        for j in range(t, n):
-            mtj = [
-                m for m in vts[j] if m != top_order[t] and not hat_g_s[top_order[t], m]
-            ]
+    hat_g = np.zeros((n, n), dtype=np.bool_)
+    for i in range(n):
+        hat_g[:,i] = np.diagonal(dsz_cor[i]) >= ATOL_EDGE
 
-            # Find the basis of/orthonormal projector onto mtj
-            if len(mtj) == 0:
-                # trivial column space => full null space
-                logging.debug(f"{j = }, rank of mtj: 0")
-                mtj_nullp = np.eye(d)
-            else:
-                cm = np.sum(dsx_cor[mtj], 0)
-                cm_eig = np.linalg.eigh(cm)
-                cm_eigval: npt.NDArray[np.floating] = cm_eig.eigenvalues
-                cm_eigvec: npt.NDArray[np.floating] = cm_eig.eigenvectors
-                cm_rank = np.sum(cm_eigval > atol_eigv, dtype=np.int_)
+    hat_g &= ~np.eye(n, dtype=np.bool_)
+    # Make the estimated graph acyclic
+    hat_g = utils.dag.closest_dag(hat_g)
+    # Find and save a topological order
+    top_order = utils.dag.topological_order(hat_g)
 
-                # We know a priori that M_{t,t} = V_{t-1} with rank t-1.
-                if j == t:
-                    cm_rank = t
+    assert top_order is not None
 
-                logging.debug(f"{j = }, rank of mtj: {cm_rank}")
-                mtj_nullb = cm_eigvec[:, :-cm_rank]
-                mtj_nullp = mtj_nullb @ mtj_nullb.T
-
-            # We check if there is any vector in null space of mtj but not in vt:
-            vtn_on_mtjn = mtj_nullp @ vt_colb
-            col_norms: npt.NDArray[np.floating] = np.linalg.norm(
-                vtn_on_mtjn, ord=2, axis=0
-            )
-            if col_norms.size != 0:
-                col_norms_argmax = np.argmax(col_norms)
-                if col_norms[col_norms_argmax] > atol_orth:
-                    # null of mtj **is not** a subspace of null of vt
-                    # implied by I^{pi_t} not in pa_{tc}(I^{pi_j}
-                    # Update $pi_t$-th row of encoder estimate.
-                    hat_enc_s_pinv_t[top_order[t], :] = (
-                        vtn_on_mtjn[:, col_norms_argmax] / col_norms[col_norms_argmax]
-                    )
-                    continue
-
-            # else:
-            # null of mtj **is** a subspace of null of vt
-            # implies $I^{\pi_{t}} \in \pa_{\mcG_{tr}}(I^{\pi_{j}}$
-            # Then there is a transitive reduction edge. Add it.
-            hat_g_s[top_order[t], top_order[j]] = True
-            if not full_rank_scores:
-                # If we are working with transitive closure, update it
-                hat_g_s[top_order[t], :] |= hat_g_s[top_order[j], :]
-
-    hat_enc_s = np.linalg.pinv(hat_enc_s_pinv_t.T)
-
-    # Normalize rows of the encoder estimate
-    hat_enc_s /= np.linalg.norm(hat_enc_s, ord=2, axis=1, keepdims=True)
-
-    return hat_g_s, hat_enc_s
+    return hat_g, top_order
 
 
-def _unmixing_procedure(
+
+def _unmixing_cov(
     x_cov: npt.NDArray[np.floating],
     dsx_cor: npt.NDArray[np.floating],
     top_order: npt.NDArray[np.int_],
     hat_enc_s: npt.NDArray[np.floating],
-    atol_edge: float,
+    hat_g_s: npt.NDArray[np.bool_],
+    ATOL_EDGE: float,
 ) -> tuple[npt.NDArray[np.bool_], npt.NDArray[np.floating]]:
     logging.info(f"Starting `_unmixing_procedure`.")
-    logging.debug(f"{atol_edge = }")
+    logging.debug(f"{ATOL_EDGE = }")
 
-    n, _, _ = dsx_cor.shape
+    '''
+    Unmixing procedure for hard interventions.
+    Stage L3 of Algorithm 1 in the paper.
+    
+    Refines the encoder and graph estimates.
+    '''
 
-    # First, build an unmixing matrix.
-    hat_z_cov = hat_enc_s @ x_cov @ hat_enc_s.T
-    unmix_mat = np.eye(n)
+    hat_enc_h = hat_enc_s.copy()
+    #hat_g_h = hat_g_s.copy()
+    n = hat_g_s.shape[0]
 
-    for k in range(n):
-        hat_z_tilde_z_cov = unmix_mat @ hat_z_cov
-        a_mat = hat_z_tilde_z_cov[1 + top_order[k]][
-            np.ix_(top_order[:k], top_order[:k])
-        ]
-        b_vec = hat_z_tilde_z_cov[1 + top_order[k]][top_order[:k], top_order[k]]
-        unmix_mat[top_order[k], top_order[:k]] = np.linalg.solve(a_mat, -b_vec)
+    ## ENCODER UPDATE
+    for t in range(1,n):
+        m = top_order[t]
+        an_m = np.where(hat_g_s[:,m])[0]
+        if len(an_m) == 0:
+            # t has no ancestors, already identified
+            continue
+        else:       
+            # get the covariance of Z for m-th int. environment
+            hat_z_cov = hat_enc_s @ x_cov[m+1] @ hat_enc_s.T
+            # solve for the unmixing vector
+            u = np.linalg.solve(hat_z_cov[an_m][:,an_m], hat_z_cov[an_m][:,m])
+            hat_enc_h[m,:] -= u @ hat_enc_s[an_m][:]
 
-    hat_enc_h = unmix_mat @ hat_enc_s
+    ## DECODER UPDATE
+    hat_dec_h = np.linalg.pinv(hat_enc_h)
+    dsz_cor = hat_dec_h.T @ dsx_cor @ hat_dec_h
 
-    # Normalize rows of the encoder estimate:
-    hat_enc_h: npt.NDArray[np.floating] = hat_enc_h / np.linalg.norm(
-        hat_enc_h, ord=2, axis=1, keepdims=True
-    )
+    ## GRAPH UPDATE
+    hat_g_h, top_order_h = _estimate_graph(dsz_cor, ATOL_EDGE)
 
-    # Next, estimate the latent graph using the new hat Z score changes.
-    hat_g_h = np.zeros((n, n), dtype=np.bool_)
-    hat_enc_h_pinvt = np.linalg.pinv(hat_enc_h).T
-    dshatz_cor = hat_enc_h_pinvt @ dsx_cor @ hat_enc_h_pinvt.T
+    return hat_enc_h, hat_g_h, top_order_h, dsz_cor
+
+def l_scale_i_full_rank_scores(
+    dsx_cor: npt.NDArray[np.floating],
+    hat_g_s: npt.NDArray[np.bool_],
+    top_order: npt.NDArray[np.int_],
+    hat_enc_n_s: npt.NDArray[np.floating],
+    ATOL_EIGV: float,
+) -> tuple[
+    npt.NDArray[np.int_],
+    npt.NDArray[np.bool_],
+    npt.NDArray[np.floating],
+    npt.NDArray[np.floating],
+    tuple[
+        npt.NDArray[np.int_],
+        npt.NDArray[np.bool_],
+        npt.NDArray[np.floating],
+        npt.NDArray[np.floating],
+    ]
+    | None,
+]:
+    '''
+    LSCALE-I for sufficiently nonlinear latent causal models.
+    Algorithm 2 in the paper.
+    '''
+
+    assert dsx_cor.ndim == 3
+    _, d, d = dsx_cor.shape
+    n = len(top_order)
+
+    hat_g_s_tr = utils.dag.transitive_reduction(hat_g_s)
+    hat_g_s_tc = utils.dag.transitive_closure(hat_g_s)
+
+    ### LATENT GRAPH ESTIMATION
+    hat_g_f = np.zeros((n, n), dtype=np.bool_)
     for i in range(n):
-        # i-th entry of the score difference is zero iff it has 0 correlation.
-        hat_g_h[:, i] = np.diagonal(dshatz_cor[i]) >= atol_edge
+        k = top_order[i]
+        for j in range(i):
+            t = top_order[j]
+            # OPTIONAL: leverage known causal relationships from previous steps
+            if hat_g_s_tr[t,k] == True:
+                hat_g_f[t,k] = 1
+                continue
+            elif hat_g_s_tc[t,k] == False:
+                hat_g_f[t,k] = 0
+                continue
 
-    # Remove the i == j case -- these are not actual edges
-    hat_g_h &= ~np.eye(n, dtype=np.bool_)
 
-    return hat_g_h, hat_enc_h, dshatz_cor
+            subspace_tk = utils.linalg.subspace_intersection_from_cor([dsx_cor[t],dsx_cor[k]],tol=ATOL_EIGV)
+            rank_tk_int = subspace_tk.shape[1]
+
+            hat_pa_tk_int = np.where(hat_g_f[:,t]*hat_g_f[:,k])[0]
+            if rank_tk_int > len(hat_pa_tk_int):
+                hat_g_f[t,k] = True 
+
+    # ENCODER ESTIMATION
+    hat_enc_f = np.zeros((n, d))
+    R_list = [dsx_cor[i] for i in range(n)]
+    for m in range(n):
+        # Estimate the encoder row by row from score differences
+        ch_m = np.where(hat_g_f[m,:])[0]
+        chplus_m = np.append(ch_m, m)
+        Q_int = utils.linalg.subspace_intersection_from_cor([R_list[i] for i in chplus_m], tol=1e-6)
+        if Q_int.shape[1] == 0:
+            # this should not happen, but if it happens, we just keep the old vector from the previous step
+            hat_enc_f[m] = hat_enc_n_s[m]
+        else:
+            # note that np.linalg.eigh returns eigenvectors in ascending order, so last basis is the strongest one.
+            hat_enc_f[m] = Q_int[:,-1]
+
+
+    return hat_g_f, hat_enc_f
